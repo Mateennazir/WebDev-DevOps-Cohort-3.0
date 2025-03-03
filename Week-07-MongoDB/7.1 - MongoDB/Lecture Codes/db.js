6d17bb92c2ac1722fc235f866287672457a45692
Mateennazir <mateennazir0@gmail.com>
2025-03-03 13:55:30 +0530

    Mongoose

--- Week-07-MongoDB/7.1 - MongoDB/Lecture Codes/db.js
+++ Week-07-MongoDB/7.1 - MongoDB/Lecture Codes/db.js
@@ -15,16 +15,16 @@ const ObjectId = mongoose.ObjectId;
 
 // Define the User schema with fields for email, password, and name
 const User = new Schema({
-    email: { type: String, unique: true }, // Make email unique to avoid duplicate entries
-    password: String,
-    name: String,
+  email: { type: String, unique: true }, // Make email unique to avoid duplicate entries
+  password: String,
+  name: String,
 });
 
 // Define the Todo schema with fields for title, done, and userId
 const Todo = new Schema({
-    title: String,
-    done: Boolean,
-    userId: ObjectId,
+  title: String,
+  done: Boolean,
+  userId: ObjectId, // Which user does it belong to.
 });
 
 // Create Mongoose models for users and todos collections using the User and Todo schemas
@@ -33,6 +33,6 @@ const TodoModel = mongoose.model("todos", Todo);
 
 // Export the User and Todo models for use in other files
 module.exports = {
-    UserModel,
-    TodoModel,
+  UserModel,
+  TodoModel,
 };

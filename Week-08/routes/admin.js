2497ee075b417a78b48b100f069b35bf7430ba82
Mateennazir <mateennazir0@gmail.com>
2025-03-05 18:05:05 +0530

    Added routes for Adim login

---
+++ Week-08/routes/admin.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", (req, res) => {
+  res.json({
+    message: "signed up",
+  });
+});
+
+adminRouter.post("/signin", (req, res) => {
+  res.json({
+    message: "Signed in",
+  });
+});
+
+adminRouter.post("/course", (req, res) => {
+  res.json({
+    message: "Signed in",
+  });
+});
+adminRouter.put("/course", (req, res) => {
+  res.json({
+    message: "Signed in",
+  });
+});
+adminRouter.get("/course/bulk", (req, res) => {
+  res.json({
+    message: "Signed in",
+  });
+});
+
+module.exports = {
+  adminRouter: adminRouter,
+};

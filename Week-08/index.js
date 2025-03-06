125e2f891862244c8a5804eab243ef4bcd2f4beb
Mateennazir <mateennazir0@gmail.com>
2025-03-06 22:43:23 +0530

    Step 7 only start if the Db is up

--- Week-08/index.js
+++ Week-08/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
@@ -9,4 +10,11 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-app.listen(3000);
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://muteebnazir8:CgboLRob8Wl9SXya@admin1.l34fi.mongodb.net/coursera-app"
+  );
+  app.listen(3000);
+  console.log("Lsitening on port 3000");
+}
+main();

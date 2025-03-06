039613f0fef598143894ffccd822f12d5fedb3dc
Mateennazir <mateennazir0@gmail.com>
2025-03-06 22:23:17 +0530

    Next is to give connection to mongodb. Schema is done

--- Week-08/db.js
+++ Week-08/db.js
@@ -1,8 +1,13 @@
 const { Schema, default: mongoose } = require("mongoose");
+mongoose.connect("");
+const ObjectId = mongoose.ObjectId;
+const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-  email: { type: String, unique: true },
+  email: { type: String, unique: true }, // Make email unique to avoid duplicate entries
   password: String,
+  firstName: String,
+  lastName: String,
 });
 
 const adminSchema = Schema({
@@ -13,14 +18,26 @@ const adminSchema = Schema({
 });
 
 const courseSchema = Schema({
-  email: { type: String, unique: true }, // Make email unique to avoid duplicate entries
-  password: String,
-  name: String,
+  title: String,
+  description: String,
+  price: Number,
+  Image: String,
+  CreatorId: ObjectId,
 });
 
-const purchaseSchema = Schema({});
+const purchaseSchema = Schema({
+  userId: String,
+  courseId: ObjectId,
+});
 
 const userModel = mongoose.model("users", userSchema);
 const adminModel = mongoose.model("admin", adminSchema);
 const courseModel = mongoose.model("course", courseSchema);
 const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.export = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
